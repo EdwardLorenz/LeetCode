855d6023cb5bd1e68c6787dd3642bea7a3a36079
Liu Qing <597586401@qq.com>
2017-09-02 23:50:32 -0400

    Update 283. Move Zeroes.java

--- 283. Move Zeroes.java
+++ 283. Move Zeroes.java
@@ -1,22 +1,12 @@
 class Solution {
     public void moveZeroes(int[] nums) {
         
-        if(nums == null || nums.length == 0){
-            return;
-        }
-        
-        int nonZeroCnt = 0;
-        
-        // round 1: non 0s
+        int index = 0;
         for(int i = 0; i < nums.length; i ++){
-            if(nums[i] != 0){
-                nums[nonZeroCnt] = nums[i];
-                nonZeroCnt ++;
-            }
+            if(nums[i] != 0) nums[index ++] = nums[i];
         }
         
-        // round 2: 0s
-        for(int i = nonZeroCnt; i < nums.length; i ++){
+        for(int i = index; i < nums.length; i ++){
             nums[i] = 0;
         }
         
